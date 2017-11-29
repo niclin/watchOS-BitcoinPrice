38a504a674e835b5f191efd8c16b652ecfc35be2
niclin <bboyceo@hotmail.com>
2017-11-30 00:53:03 +0800

    Initial Commit

---
+++ BTCPrice WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BTCPrice WatchKit Extension
+//
+//  Created by Nic on 2017/11/30.
+//  Copyright © 2017年 Nic. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
