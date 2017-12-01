4c5b3f38fe9f375ae18ce45a7bd42ff86a280303
niclin <bboyceo@hotmail.com>
2017-12-02 01:02:22 +0800

    Fetch poloniex api ok

--- BTCPrice WatchKit Extension/InterfaceController.swift
+++ BTCPrice WatchKit Extension/InterfaceController.swift
@@ -27,5 +27,41 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var cryptoCurrencyLabel: WKInterfaceLabel!
+    
+    // MARK: - Interactions
+    
+    @IBAction func getPrice() {
+        fetchPrice()
+    }
+    
+    // MARK: - Function
+    
+    func fetchPrice(){
+        
+        // Test use Bitoex exchange
+        guard let url = URL(string: "https://poloniex.com/public?command=return24hVolume") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
+    
 
 }
